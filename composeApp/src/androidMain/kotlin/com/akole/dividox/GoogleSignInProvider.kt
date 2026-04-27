52b5ed5de546bc3f740954b8fca2820c44b4d1e2
Camarena, Javier <javier.camtri@gmail.com>
2026-04-27 16:12:35 +0200

    DVX-TK-013 Fix Google Account issue

--- composeApp/src/androidMain/kotlin/com/akole/dividox/GoogleSignInProvider.kt
+++ composeApp/src/androidMain/kotlin/com/akole/dividox/GoogleSignInProvider.kt
@@ -5,8 +5,10 @@ import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.exceptions.GetCredentialCancellationException
+import androidx.credentials.exceptions.NoCredentialException
 import com.akole.dividox.common.auth.data.GoogleSignInLauncher
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 
 private const val WEB_CLIENT_ID =
@@ -15,29 +17,66 @@ private const val WEB_CLIENT_ID =
 fun initGoogleSignIn(context: Context) {
     GoogleSignInLauncher.signInProvider = {
         val credentialManager = CredentialManager.create(context)
+        getGoogleIdToken(credentialManager, context)
+    }
+}
 
-        val googleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(false)
-            .setServerClientId(WEB_CLIENT_ID)
-            .setAutoSelectEnabled(false)
-            .build()
-
-        val request = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
-            .build()
-
+private suspend fun getGoogleIdToken(
+    credentialManager: CredentialManager,
+    context: Context,
+): String? {
+    return try {
+        requestGoogleIdToken(credentialManager, context, filterByAuthorized = false)
+    } catch (_: NoCredentialException) {
+        // Fall back to the full Sign-In with Google bottom sheet
         try {
-            val response = credentialManager.getCredential(context, request)
-            val credential = response.credential
-            if (credential is CustomCredential &&
-                credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
-            ) {
-                GoogleIdTokenCredential.createFrom(credential.data).idToken
-            } else {
-                null
-            }
+            requestSignInWithGoogle(credentialManager, context)
         } catch (_: GetCredentialCancellationException) {
             null
         }
+    } catch (_: GetCredentialCancellationException) {
+        null
     }
 }
+
+private suspend fun requestGoogleIdToken(
+    credentialManager: CredentialManager,
+    context: Context,
+    filterByAuthorized: Boolean,
+): String? {
+    val option = GetGoogleIdOption.Builder()
+        .setFilterByAuthorizedAccounts(filterByAuthorized)
+        .setServerClientId(WEB_CLIENT_ID)
+        .setAutoSelectEnabled(false)
+        .build()
+
+    val request = GetCredentialRequest.Builder()
+        .addCredentialOption(option)
+        .build()
+
+    val response = credentialManager.getCredential(context, request)
+    return response.credential.toGoogleIdToken()
+}
+
+private suspend fun requestSignInWithGoogle(
+    credentialManager: CredentialManager,
+    context: Context,
+): String? {
+    val option = GetSignInWithGoogleOption.Builder(WEB_CLIENT_ID).build()
+
+    val request = GetCredentialRequest.Builder()
+        .addCredentialOption(option)
+        .build()
+
+    val response = credentialManager.getCredential(context, request)
+    return response.credential.toGoogleIdToken()
+}
+
+private fun androidx.credentials.Credential.toGoogleIdToken(): String? =
+    if (this is CustomCredential &&
+        type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
+    ) {
+        GoogleIdTokenCredential.createFrom(data).idToken
+    } else {
+        null
+    }
